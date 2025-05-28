a668fb4dbe15d601adf2ec4adb30b1adbb147477
100QN <83006473+100QNKolev@users.noreply.github.com>
2025-05-28 13:40:17 +0300

    created-router

---
+++ src/router.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import itemsRouter from './controllers/itemsController';
+
+const router = express.Router(); // Create a new router
+
+router.use(express.json()); // Parse JSON request bodies
+router.use('/', itemsRouter); // Use the items router
+
+export default router; // Export the router
